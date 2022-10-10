27875556f6598560be804878fca2f933e38f8c8e
karthik <karthik.cs1720@gmail.com>
2022-10-10 21:25:46 +0530

    api url updated

--- src/httpService.js
+++ src/httpService.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
 // Add a request interceptor
-// axios.interceptors.request.use(
-//   function (config) {
-//     // Do something before request is sent
-//     // OR config.headers.common['Authorization'] = `Bearer ${your_token}`;
-//   },
-//   function (error) {
-//     // Do something with request error
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+
+    // OR config.headers.common['Authorization'] = `Bearer ${your_token}`;
+    config.baseURL = "https://karthik-crypto.herokuapp.com";
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 
 const http = {
   get: axios.get,

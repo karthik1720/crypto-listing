280b330fbf4bbeac5bf208cc3636776172929328
karthik <karthik.cs1720@gmail.com>
2022-10-13 10:44:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
